c1a50d6583b225989303acb51b8d0a825057920b
kholoud Shaarawy <kholoudshaarawy.sb@gmail.com>
2026-06-06 12:45:57 +0300

    U

--- src/main/java/com/ex/configg/SecurityConfig.java
+++ src/main/java/com/ex/configg/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ex.configg;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,15 +20,22 @@ public class SecurityConfig {
                 .httpBasic()
                 .and()
                 .authorizeRequests()
-                //.anyRequest().authenticated()
+                //  .anyRequest().authenticated()
                 //.anyRequest().permitAll() //makes the endpoint public, Allow any request to pass, even if there is no Authentication. Authorization rule = allow everyone Authentication required = no, .
-               //.anyRequest().denyAll() //comes after specific request rules that define authentication or roles. Close any endpoint that I did not explicitly define.
-               //.anyRequest().hasAuthority("read")
-               // .anyRequest().hasAnyAuthority("read","write")
-               // .anyRequest().hasRole("ADMIN")
-              //  .anyRequest().hasAnyRole("ADMIN", "MANAGER")
-               // .anyRequest().access("isAuthenticated() and hasAuthority('read')")//SpEL expression -> A condition written as text, which Spring reads and interprets.T/F //a custom authorization decision (Role + Authority)
-              //  .mvcMatchers()
+                //.anyRequest().denyAll() //comes after specific request rules that define authentication or roles. Close any endpoint that I did not explicitly define.
+                //.anyRequest().hasAuthority("read")
+                // .anyRequest().hasAnyAuthority("read","write")
+                // .anyRequest().hasRole("ADMIN")
+                //  .anyRequest().hasAnyRole("ADMIN", "MANAGER")
+                // .anyRequest().access("isAuthenticated() and hasAuthority('read')")//SpEL expression -> A condition written as text, which Spring reads and interprets.T/F //a custom authorization decision (Role + Authority)
+                //.mvcMatchers("/test1","/test3").authenticated() //No specific role is required. The only requirement is that the user must be authenticated (logged in).
+                //.mvcMatchers("/test2").hasAuthority("read")
+                // .mvcMatchers("/demo/*").hasAuthority("read") //One level after the prefix, /demo/test
+                //  .mvcMatchers("/demo/**").hasAuthority("read") //All levels after the prefix, /demo/test  , /demo/user/profile   , /demo/user/10/orders
+                //.mvcMatchers("/demo/*/*/*").hasAuthority("read") //endpoint with 3 levels after the prefix, /demo/user/profile
+                .mvcMatchers(HttpMethod.GET, "/demo/**").hasAuthority("read")
+                // .regexMatchers("/api/v[0-9]+/users/.*") ///api/v10/users/profile
+                .anyRequest().authenticated()
                 .and()
                 .build();
     }
@@ -35,13 +43,21 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() { // adapter between Spring Security and data
         var u = new InMemoryUserDetailsManager();
-        var user = User.withUsername("roz")
-                   .password(passwordEncoder()
-                   .encode("123"))
-                   .authorities("read")
-                   .roles("ADMIN")
-                   .build();
-        u.createUser(user);
+        var user1 = User.withUsername("roz")
+                .password(passwordEncoder()
+                        .encode("123"))
+                .authorities("read")
+                //   .roles("ADMIN")
+                .build();
+        u.createUser(user1);
+
+        var user2 = User.withUsername("jo")
+                .password(passwordEncoder()
+                        .encode("1234"))
+                .authorities("write", "delete")
+                //   .roles("ADMIN")
+                .build();
+        u.createUser(user2);
         return u;
     }
 
--- src/main/java/com/ex/controllerr/Test.java
+++ src/main/java/com/ex/controllerr/Test.java
@@ -1,14 +1,33 @@
 package com.ex.controllerr;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/demo") //prefix
 public class Test {
 
-    @GetMapping("/demo")
-    public String demo() {
+    @GetMapping("/test1") //.mvcMatchers("/demo/*").hasRole("ADMIN")
+    public String test1() {
 
-        return "Demo!";
+        return "test1";
+    }
+
+
+    @GetMapping("/test2")  //.mvcMatchers("/demo/*").hasRole("ADMIN")
+    public String test2() {
+
+        return "test2";
+    }
+    @GetMapping("/users/profile") //.mvcMatchers("/demo/*/*").hasRole("ADMIN")
+    public String profile() {
+        return "profile";
+    }
+
+    @PostMapping("/users/10/orders") //.mvcMatchers("/demo/*/*/*").hasRole("ADMIN")
+    public String orders() {
+        return "orders";
     }
 }
